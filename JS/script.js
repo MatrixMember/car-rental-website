c699cbf1076dfa6e86a5b83a137ccc779cc1cfdc
Aleksander Sarna <oleksarna@gmail.com>
2023-06-17 17:24:46 +0200

    function added

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,23 @@
-console.log("Hello!");
 
-let button = document.querySelector(".button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".button__themeName");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("body__dark");
-
-    themeName.innerText = body.classList.contains("body__dark") ? "ciemny" : "jasny"
+{
+    const welcome = () => {
+        console.log("Hello!");
+    };
 
 
-});
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const themeName = document.querySelector(".button__themeName");
+        body.classList.toggle("body__dark");
+        themeName.innerText = body.classList.contains("body__dark") ? "ciemny" : "jasny"
+    };
+
+    const init = () => {
+        const button = document.querySelector(".button");
+        button.addEventListener("click", toggleBackground);
+      
+    };
+
+    welcome();
+    init();
+};
